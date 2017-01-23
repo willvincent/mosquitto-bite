7f6764810285d2b5c1c8470fbb5c09d3d08033bf
Will Vincent <will.vincent@ffwagency.com>
2017-01-23 01:44:48 -0600

    playing with notifications.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,23 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const notifier = require('node-notifier');
+
+
+let fire = function() {
+  notifier.notify({
+    title: 'Test notification!',
+    message: 'This is a test notification. Neat, huh?',
+    sound: true,
+    wait: true
+  }, function (err, response) {
+  });
+};
+
+
+fire();
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("notify").addEventListener("click", fire);
+})
